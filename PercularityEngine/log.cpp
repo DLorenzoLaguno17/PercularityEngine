98e058aa93c6709e7cb89a2d05fd06daca608805
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2019-10-03 17:42:57 +0200

    Revert "minor"
    This reverts commit 0d8d580d44f82e632dd514ad86f96e00d07906e1.

--- PercularityEngine/log.cpp
+++ PercularityEngine/log.cpp
@@ -14,6 +14,6 @@ void log(const char file[], int line, const char* format, ...)
 	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
 	OutputDebugString(tmp_string2);
 
-	/*if (App != nullptr)
-		App->gui->log_list.push_back(tmp_string);*/
+	if (App != nullptr)
+		App->gui->log_list.push_back(tmp_string);
 }
